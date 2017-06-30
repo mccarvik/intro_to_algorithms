03f0a899a114155656e5b8b6b2a5efc9868503e6
mccarvik <mccarviks@gmail.com>
2017-06-30 19:39:31 +0000

    making some progress

--- 5ch/hire_assistant.js
+++ 5ch/hire_assistant.js
@@ -1,3 +1,4 @@
+var rand_funcs = require('./rand_funcs.js');
 
 function hire_assistant(arr) {
     var best = -1;
@@ -14,67 +15,15 @@ function hire_assistant(arr) {
     return cost;
 }
 
-// Randomization function
-// Randomly generates numbers, sorts them, then applies the indices to randomly
-// sort the original array
-function permute_by_sorting(arr) {
-    var unsorted = [];
-    for (var i=0; i<arr.length; i++) {
-        unsorted.push(Math.random() *  Math.pow(3, arr.length));
-    }
-    var ret_arr = [];
-    var sorted = sort(unsorted);
-    for (var i=0; i<sorted.length; i++) {
-        ret_arr.push(arr[unsorted.indexOf(sorted[i])]);
-    }
-    return ret_arr;
-}
-
-function sort(arr) {
-  return arr.concat().sort(compare);
-}
-
-function compare(a, b){
-    if (a > b) { return 1; }
-    else { return -1; }
-}
-
-// Randomization function
-// randomly selects indices to reconstruct the list
-function randomize_in_place(arr) {
-    var ret_arr = [];
-    
-    while (arr.length > 0) {
-        var i = parseInt(Math.random() * arr.length);
-        ret_arr.push(arr[i]);
-        arr.splice(i, 1);
-    }
-    return ret_arr
-}
-
-
-
-function rand_list(len, min, max) {
-    var ret = [];
-    var i = 0;
-    while (i < len) {
-        var val = (parseInt(Math.random() * (max-min)) + min);
-        if (ret.indexOf(val) === -1) {
-            ret.push(val);
-            i++;
-        }
-    }
-    return ret;
-}
-
-var list = rand_list(10,0,10);
-// list = permute_by_sorting(list)
-// console.log(hire_assistant(list));
-
-console.log(list);
-var new_list = randomize_in_place(list)
-console.log(new_list);
-
 // Each interviewee has 1/i chance of being better than the candidates before them
 // On average we hire Sigma : 1/ i, for 1 to i-1, 
 // = ln(n) hires
+
+var list = rand_funcs.rand_list(10,0,10);
+console.log(hire_assistant(list));
+
+
+console.log(list);
+var new_list = rand_funcs.randomize_in_place(list)
+console.log(new_list);
+
---
+++ 5ch/rand_funcs.js
@@ -0,0 +1,53 @@
+
+// Randomization function
+// Randomly generates numbers, sorts them, then applies the indices to randomly
+// sort the original array
+exports.permute_by_sorting = function (arr) {
+    var unsorted = [];
+    for (var i=0; i<arr.length; i++) {
+        unsorted.push(Math.random() *  Math.pow(3, arr.length));
+    }
+    var ret_arr = [];
+    var sorted = sort(unsorted);
+    for (var i=0; i<sorted.length; i++) {
+        ret_arr.push(arr[unsorted.indexOf(sorted[i])]);
+    }
+    return ret_arr;
+}
+
+function sort(arr) {
+  return arr.concat().sort(compare);
+}
+
+function compare(a, b){
+    if (a > b) { return 1; }
+    else { return -1; }
+}
+
+// Randomization function
+// randomly selects indices to reconstruct the list
+exports.randomize_in_place = function (arr) {
+    var ret_arr = [];
+    
+    while (arr.length > 0) {
+        var i = parseInt(Math.random() * arr.length);
+        ret_arr.push(arr[i]);
+        arr.splice(i, 1);
+    }
+    return ret_arr
+}
+
+
+
+exports.rand_list = function (len, min, max) {
+    var ret = [];
+    var i = 0;
+    while (i < len) {
+        var val = (parseInt(Math.random() * (max-min)) + min);
+        if (ret.indexOf(val) === -1) {
+            ret.push(val);
+            i++;
+        }
+    }
+    return ret;
+}
