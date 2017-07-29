1996f92bc546af5bad42f6c59e3f077b4abcda67
mccarvik <mccarviks@gmail.com>
2017-07-29 14:16:18 +0000

    fixed count sort

--- 8ch/count_sort.js
+++ 8ch/count_sort.js
@@ -1,25 +1,31 @@
 var rand_funcs = require('../5ch/rand_funcs.js');
 
 function count_sort(A, k) {
-    var C = [];
+    // k = max of array
+    var C = new Array(k+2).join('0').split('').map(parseFloat)
     var B = [];
+    
     for (var j=0; j < A.length; j++) {
         C[A[j]] = C[A[j]] + 1;
     }
     
-    for (var i=0; i < k; i++) {
+    console.log(C);
+    
+    for (var i=1; i < k+1; i++) {
         C[i] = C[i] + C[i-1];
     }
     
     console.log(C);
-    for (var j=A.length; j > -1 ; j--) {
+    for (var j=A.length-1; j > -1 ; j--) {
         B[C[A[j]]] = A[j];
         C[A[j]]--;
     }
     
+    // remove empty first element
+    B.shift()
     return B;
 }
 
-var list = rand_funcs.rand_list(20,0,100);
+var list = rand_funcs.rand_list(10,0,15);
 console.log(list);
-console.log(count_sort(list,list.length));
+console.log(count_sort(list, Math.max.apply(Math, list)));
