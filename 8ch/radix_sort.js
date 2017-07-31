4c79c1e64a6227f1eef22ec91a04bfa01cbe43fe
mccarvik <mccarviks@gmail.com>
2017-07-31 20:29:43 +0000

    get the algorithm, need to implement

---
+++ 8ch/radix_sort.js
@@ -0,0 +1,7 @@
+var rand_funcs = require('../5ch/rand_funcs.js');
+
+function radix_sort(A, d) {}
+
+var list = rand_funcs.rand_list(20,10,99);
+console.log(list);
+console.log(radix_sort(list, 2));
