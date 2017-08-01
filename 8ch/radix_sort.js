717b03eb477f9ac3e37ce16e41bcb0651263a20a
mccarvik <mccarviks@gmail.com>
2017-08-01 15:22:58 +0000

    finished radix sort

--- 8ch/radix_sort.js
+++ 8ch/radix_sort.js
@@ -1,7 +1,29 @@
 var rand_funcs = require('../5ch/rand_funcs.js');
 
-function radix_sort(A, d) {}
+function radix_sort(A, d) {
+    for (var i=d-1; i>-1; i--) {
+        console.log(A)
+        A = bubble_sort(A, i)
+    }
+    return A
+}
+
+
+function bubble_sort(A, d) {
+    for (var i=0; i < A.length; i++) {
+        for (var j=A.length-1; j > i; j--) {
+            if (parseInt(A[j].toString()[d]) < parseInt(A[j-1].toString()[d])) {
+                var holder = A[j];
+                A[j] = A[j-1];
+                A[j-1] = holder;
+                // console.log(A);
+            }
+        }
+    }
+    return A;
+}
+
 
 var list = rand_funcs.rand_list(20,10,99);
-console.log(list);
+console.log('Original List: ' + list);
 console.log(radix_sort(list, 2));
