ce2e62a5207268c15ebd543e2eca1bc873ecd1a7
mccarvik <mccarviks@gmail.com>
2019-02-07 02:19:03 +0000

    start ch18

--- 17ch/aggregate_analysis.py
+++ 17ch/aggregate_analysis.py
@@ -1,19 +1,20 @@
 """
 Function for aggregate analysis, ch 17.1
 """
-import pdb
-
 
 def multi_pop(stack, k):
     """
     pops k values from stack
     """
-    for i in range(k):
+    for i_ct in range(k):
         stack.pop()
     return stack
-    
+
 
 def binary_inc(array):
+    """
+    performs a binary increment by 1
+    """
     i_ct = 0
     while i_ct < len(array) and array[i_ct] == 1:
         array[i_ct] = 0
@@ -24,6 +25,9 @@ def binary_inc(array):
 
 
 def table_insert(T, x):
+    """
+    inserts into a table and resizes if necessary
+    """
     if len(T) == 0:
         T = [0]
     if sum(T) == len(T):
@@ -33,51 +37,53 @@ def table_insert(T, x):
             T_new[i] = T[i]
         T = T_new
     # insert x in the next slot
-    for i in range(len(T)):
-        if T[i] == 0:
-            T[i] =x 
+    for i_ct in range(len(T)):
+        if T[i_ct] == 0:
+            T[i_ct] = x
             break
     print(T)
     return T
 
 
 def table_delete(T):
+    """
+    deletes an item from the table and resizes if necessary
+    """
     # table empty, nothing to delete
     if len(T) == 0:
         return T
-        
+
     # otherwise delete the item
-    for i in range(len(T)-1, -1, -1):
-        if T[i] == 1:
-            T[i] = 0
-            break 
-    
+    for i_ct in range(len(T)-1, -1, -1):
+        if T[i_ct] == 1:
+            T[i_ct] = 0
+            break
+
     # check if we should contract - load factor of 1/4
     if sum(T) < len(T) / 4:
         T_new = int(len(T) / 2) * [0]
-        for i in range(len(T_new)):
-            T_new[i] = T[i]
+        for i_ct in range(len(T_new)):
+            T_new[i_ct] = T[i_ct]
         T = T_new
     print(T)
     return T
 
 
 if __name__ == '__main__':
-    STK = [1, 2, 3, 4, 5, 6]
+    # STK = [1, 2, 3, 4, 5, 6]
     # multi pop function
     # print(multi_pop(STK, 2))
-    
+
     # increment analysis
     # array = [0] * 8
     # for inc in range(16):
     #     print(array)
     #     array = binary_inc(array)
-    
+
     # Dynamic Tables
     T = []
-    for i in range (20):
+    for i in range(20):
         T = table_insert(T, 1)
-    
-    for i in range (14):
-        T = table_delete(T)
 
+    for i in range(14):
+        T = table_delete(T)
---
+++ 18ch/b_trees.py
@@ -0,0 +1,20 @@
+
+
+class b_tree():
+    """
+    class to represent a b_tree
+    """
+    def __init__(self):
+        self.leaf = True
+        self.n = 0
+        self.root = True
+
+
+def b_tree_search(T, x, k):
+    pass
+
+
+if __name__ == '__main__':
+    tree = b_tree()
+    print(tree.root)
+    
