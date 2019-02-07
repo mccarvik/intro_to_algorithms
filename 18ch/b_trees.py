37ec2325555d52c09801d966a8a74292d903dde9
mccarvik <mccarviks@gmail.com>
2019-02-07 22:49:32 +0000

    getting there

--- 18ch/b_trees.py
+++ 18ch/b_trees.py
@@ -1,20 +1,44 @@
+"""
+script to simulate B-Tree algorithms
+"""
 
+# sets the static value of t to split on
+SPLIT_T = 4
 
-class b_tree():
+class BTree(object):
     """
     class to represent a b_tree
     """
     def __init__(self):
+        """
+        Constructor
+        """
         self.leaf = True
         self.n = 0
-        self.root = True
+        self.val = None
+        self.chd = []
 
+    def search(self, x, k):
+        """
+        Function to search trhough B-Tree
+        """
+        pass
 
-def b_tree_search(T, x, k):
-    pass
+    def split_child(self, x, i):
+        """
+        Splits the node x, on the ith child
+        """
+        z = BTree()
+        y = x.chd[i]
+        z.leaf = y.leaf
+  
+    def insert(self, k):
+        """
+        Inserts a node into a tree
+        """
+        pass
 
 
 if __name__ == '__main__':
-    tree = b_tree()
-    print(tree.root)
-    
+    ROOT = BTree()
+    print(ROOT)
