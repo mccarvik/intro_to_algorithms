2ee7296f4f270464f1ed67708e858c87e4aac7a3
Ubuntu <ubuntu@ip-172-31-91-88.ec2.internal>
2019-04-17 21:32:15 +0000

    finished ch18

--- 18ch/b_trees.py
+++ 18ch/b_trees.py
@@ -17,13 +17,15 @@ class BTreeNode(object):
     def __init__(self, leaf=False):
         self.leaf = leaf
         self.keys = []
-        self.c    = []
+        self.c = []
 
     def __str__(self):
         if self.leaf:
-            return "Leaf BTreeNode with {0} keys\n\tK:{1}\n\tC:{2}\n".format(len(self.keys), self.keys, self.c)
-        else:
-            return "Internal BTreeNode with {0} keys, {1} children\n\tK:{2}\n\n".format(len(self.keys), len(self.c), self.keys)
+            return "    Leaf BTreeNode with {0} keys\n\tK:{1}\n".format(len(self.keys), self.keys)
+        msg = "Internal BTreeNode with {0} keys, {1} children\n\tK:{2}\n\n".format(len(self.keys), len(self.c), self.keys)
+        for child in self.c:
+            msg += child.__str__() + "\n"
+        return msg
 
 
 class BTree(object):
@@ -38,9 +40,9 @@ class BTree(object):
         """
         Insert a value k into the B-Tree
         """
-        pdb.set_trace()
         r = self.root
-        if len(r.keys) == (2*self.t) - 1:     # keys are full, so we must split
+        if len(r.keys) == (2*self.t) - 1:
+            # keys are full, so we must split
             s = BTreeNode()
             # set root to new node
             self.root = s
@@ -54,7 +56,6 @@ class BTree(object):
             self._insert_nonfull(r, k)
 
     def _insert_nonfull(self, x, k):
-        pdb.set_trace()
         i = len(x.keys) - 1
         if x.leaf:
             # if x is a leaf, insert k into x
@@ -64,11 +65,11 @@ class BTree(object):
                 i -= 1
             x.keys[i+1] = k
         else:
-            # determines the child to conitnue the recursion
+            # determines the child to continue the recursion
             while i >= 0 and k < x.keys[i]:
                 i -= 1
             i += 1
-            # determines would descend to a full child then need to split
+            # if a full child then need to split
             if len(x.c[i].keys) == (2*self.t) - 1:
                 self._split_child(x, i)
                 if k > x.keys[i]:
@@ -77,15 +78,16 @@ class BTree(object):
             self._insert_nonfull(x.c[i], k)
 
     def _split_child(self, x, i):
-        pdb.set_trace()
         # x node to be split
         t = self.t
         # y is x's ith child
         y = x.c[i]
+        # create new empty node
         z = BTreeNode(leaf=y.leaf)
 
-        # slide all children of x to the right and insert z at i+1.
+        # slide all children of x to the right and insert empty node at i+1.
         x.c.insert(i+1, z)
+        # take middle key of node to be split and make it key of new node
         x.keys.insert(i, y.keys[t-1])
 
         # keys of z are t to 2t - 1,
@@ -120,29 +122,35 @@ class BTree(object):
             elif x.leaf:
                 # no match in keys, and is leaf ==> no match exists
                 return None
-            else:
-                # search children
-                return self.search(k, x.c[i])
-        else:
-            # no node provided, search root of tree
-            return self.search(k, self.root)
+            # search children
+            return self.search(k, x.c[i])
+        # no node provided, search root of tree
+        return self.search(k, self.root)
 
 
 if __name__ == '__main__':
     ROOT = BTree(3)
-    ROOT.insert('A')
     ROOT.insert('B')
-    ROOT.insert('C')
-    ROOT.insert('D')
-    ROOT.insert('E')
-    ROOT.insert('F')
     ROOT.insert('G')
-    ROOT.insert('H')
+    ROOT.insert('C')
+    ROOT.insert('K')
+    ROOT.insert('E')
+    ROOT.insert('Y')
+    ROOT.insert('F')
+    ROOT.insert('A')
+    ROOT.insert('L')
+    ROOT.insert('V')
     ROOT.insert('I')
     ROOT.insert('J')
-    ROOT.insert('K')
-    ROOT.insert('L')
+    ROOT.insert('X')
+    ROOT.insert('D')
+    ROOT.insert('Z')
+    ROOT.insert('U')
+    ROOT.insert('R')
+    ROOT.insert('S')
+    ROOT.insert('T')
     
+
 
     # search feature
     # ROOT.search('G')
