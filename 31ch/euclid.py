bdd25dea88f9c0eccb3b30b3e08062acf6efccb8
EC2 Default User <ec2-user@ip-172-31-91-31.ec2.internal>
2019-11-06 02:38:52 +0000

    finished ch 31 and 32, getting there

--- 31ch/euclid.py
+++ 31ch/euclid.py
@@ -1,4 +1,18 @@
+from fractions import gcd
 
+def pollards_rho(n):
+    x = 2; y = 2; d = 1
+    f = lambda x: (x**2 + 1) % n
+    while d == 1:
+        x = f(x); y = f(f(y))
+        d = gcd(abs(x-y), n)
+        print(x)
+    if d != n:
+        return d
+
+n = 47508355408452438672873207975244607143697401672601919005182981061393486991065174351724507009558369983282837049901
+p = pollards_rho(n)
+print('{} = {} * {}'.format(n, p, n/p))
 
 def extended_euclid(a, b):
     if b == 0:
--- 32ch/string_matching.py
+++ 32ch/string_matching.py
@@ -1,5 +1,6 @@
 import pdb
 
+
 def basic_matching(t, p):
     n = len(t)
     m = len(p)
@@ -24,18 +25,39 @@ def compute_prefix_function(p):
     return pi[1:]
 
 
-def compute_transition_function(p, s):
-    m = len(p)
-    d = {}
-    for q in range(m + 1):
-        for a in s:
-            k = min(m + 1, q + 2)
-            while k > 0:
-                k -= 1
-                if p[:k] == (p[:q] + a)[-k:]:
-                    break
-            d[(q, a)] = k
-    return d
+def KnuthMorrisPratt(text, pattern):
 
+    '''Yields all starting positions of copies of the pattern in the text.
+Calling conventions are similar to string.find, but its arguments can be
+lists or iterators, not just strings, it returns all matches, not just
+the first one, and it does not need the whole text in memory at once.
+Whenever it yields, it will have read the text exactly up to and including
+the match that caused the yield.'''
+
+    # allow indexing into pattern and protect against change during yield
+    pattern = list(pattern)
+
+    # build table of shift amounts
+    shifts = [1] * (len(pattern) + 1)
+    shift = 1
+    for pos in range(len(pattern)):
+        while shift <= pos and pattern[pos] != pattern[pos-shift]:
+            shift += shifts[pos-shift]
+        shifts[pos+1] = shift
+
+    # do the actual search
+    startPos = 0
+    matchLen = 0
+    for c in text:
+        while matchLen == len(pattern) or \
+              matchLen >= 0 and pattern[matchLen] != c:
+            startPos += shifts[matchLen]
+            matchLen -= shifts[matchLen]
+        matchLen += 1
+        if matchLen == len(pattern):
+            yield startPos
+        
 if __name__ == '__main__':
     basic_matching("abcdefghi", "def")
+    for i in KnuthMorrisPratt("abcdefghidef", "def"):
+        print("Match at {}".format(i))
